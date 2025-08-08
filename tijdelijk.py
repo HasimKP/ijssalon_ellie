1c77c5b3d4f40591a12114fb88f5c4eba0f32670
Hasim <hasim.korkmaz@Prorail.nl>
2025-08-08 15:48:30 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,21 @@
-prijzen = {
-    "aardbei": 3,
-    "Vanille": 4,
-    "chocolade": 5,
-    }
-aanbieding = 0.8 * prijzen["aardbei"]
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding} "
-reclame_tekst2 = reclame_tekst[:62]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split(" ")
-for el in reclame_tekst4:
-    if len(el) < 4:
-        print(el.lower())
-    else:
-        print(el.upper())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "Vanille": 4,
+        "chocolade": 5,
+        }
+    aanbieding = 0.8 * prijzen["aardbei"]
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding} "
+    reclame_tekst2 = reclame_tekst[:62]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split(" ")
+    for el in reclame_tekst4:
+        if len(el) < 4:
+            print(el.lower())
+        else:
+            print(el.upper())
+
+decoreer("Aanbieding")
+print_aanbieding()
